5078578b9727681d863a7fa62ba830b175fd8eae
Jdentity <identityjang@gmail.com>
2022-03-24 18:42:26 +0900

    ✅ Add setupProxy Server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [data, setData] = useState("");
+  const callApi = async () => {
+    axios.get("/api")
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data.test);
+      })
+  };
+
+  useEffect(() =>{
+    callApi();
+  }, []);
+
   return (
     <div>
-      test
+      {data}
     </div>
   );
 }
--- server/Router/test.js
+++ server/Router/test.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send({test: "hello world!"});
+  res.send({ test: "hello world!" });
 })
 
 module.exports = router;
