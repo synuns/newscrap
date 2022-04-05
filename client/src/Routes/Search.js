9c90399adc306d0e5abb826dad4a9602b5673c96
Jdentity <identityjang@gmail.com>
2022-04-05 19:33:41 +0900

    ✨ Add search page
    search page shows news search results

--- client/src/Routes/Search.js
+++ client/src/Routes/Search.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import axios from "axios";
+import { Container } from '@mui/material';
+import NewsCard from '../Components/NewsCard';
 
 const Search = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [data, setData] = useState("");
+  const query = searchParams.get('query');
+
+  const callApi = async () => {
+    axios.get(`/api/news/search?query=${query}`)
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data.value);
+      })
+  };
+
+  useEffect(() =>{
+    callApi(query);
+  }, []);
+
   return (
-    <div>News Search</div>
+    <Container>
+      {data && data.map((news, idx) => (
+        <NewsCard key={idx} news={news} />
+      ))}
+    </Container>
   )
 }
 
