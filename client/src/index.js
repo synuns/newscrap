cf1390e6c7f6509b38a8ae270fc47faefd9f8633
Jdentity <identityjang@gmail.com>
2022-04-09 17:36:22 +0900

    ✅ Add react-query
    add QueryclientProvider in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { QueryCache, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    refetchOnWindowFocus: false,
+    queries: {
+      retry: 0,
+      suspense: true,
+    }
+  },
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      console.log(error, query);
+    },
+    onSuccess: data => {
+      console.log(data)
+    }
+  })
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
