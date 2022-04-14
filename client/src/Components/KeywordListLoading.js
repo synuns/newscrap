4c0fb06d8954c61753e25a7893d21edd36fa24c3
Jdentity <identityjang@gmail.com>
2022-04-14 17:45:37 +0900

    🐛 fix no component key
    add key int the KeywordListLoading

--- client/src/Components/KeywordListLoading.js
+++ client/src/Components/KeywordListLoading.js
@@ -4,14 +4,17 @@ import React from 'react';
 const KeywordListLoading = () => {
   let skeletons = [];
 
-  const skeleton = (
-    <Accordion sx={{ height: '40px'}}>
+  const skeleton = (key) => (
+    <Accordion 
+      key={key} 
+      sx={{ height: '40px'}}
+    >
       <Skeleton sx={{ mx: 2, height: '100%' }} />
     </Accordion>
   );
 
   for(let i=0; i<20; i++){
-    skeletons.push(skeleton);
+    skeletons.push(skeleton(i));
   };
 
   return (
