85e6aeaf864c5a03d97631f1f22418aa77ed1d33
Jdentity <identityjang@gmail.com>
2022-04-14 17:46:51 +0900

    🤡 Add major news mock data
    apply mock data temporally

--- client/src/api/NewsAPI.js
+++ client/src/api/NewsAPI.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 const NewsAPI = async () => {
-  const newsData = await axios.get("/api/news")
+  // const newsData = await axios.get("/api/news")
+  const newsData = await axios.get("http://localhost:3000/data/majorNewsData.json")
     .then((res) => {
       return res.data;
     })
     .catch((err) => {
       return err;
     });
+  
   return newsData;
 }
 
