f946b781baad7af2525a15d6a67d2b0687cdb674
Jdentity <identityjang@gmail.com>
2022-04-19 19:38:36 +0900

    ✅ split keywords by date
    add function splitKeywordByDate
    use moment, lodash

--- client/src/Components/KeywordList.js
+++ client/src/Components/KeywordList.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQuery } from 'react-query';
+import _ from 'lodash';
+import moment from 'moment';
 import TrendsAPI from '../api/TrendsAPI';
 import fromNow from '../utils/fromNow';
 import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Typography } from '@mui/material';
@@ -7,54 +9,70 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const KeywordList = () => {
   const { data } = useQuery(['trends'], TrendsAPI);
-
   const [expanded, setExpanded] = useState(false);
-  const [selected, setSelected] = useState("");
-  // const { data, isLoading, error } = useQuery(
-  //   ['relatedNews', selected], 
-  //   () => NewsSearchAPI(selected),
-  //   {
-  //     enabled: !!selected,
-  //   }
-  // );
+  const [keywordByDate, setKeywordByDate] = useState({});
 
-  const handleChange = (index, keyword) => (event, isExpanded) => {
-    setExpanded(isExpanded ? index : false);
-    setSelected(keyword);
+  const handleChange = (keywordTitle) => (_, isExpanded) => {
+    setExpanded(isExpanded ? keywordTitle : false);
   };
-  
+
+  const splitKeywordByDate = (keywords) => {
+    let keywordByDate = {};
+    _.forEach(keywords, (keyword) => {
+      const dateForm = 'YYYY-MM-DD';
+      const keywordDate = moment(keyword.isoDate, dateForm).format(dateForm);
+      if(!Object.hasOwn(keywordByDate, keywordDate)){
+        keywordByDate[keywordDate] = [];
+      } 
+      keywordByDate[keywordDate].push(keyword);
+    })
+    return keywordByDate;
+  }
+
+  useEffect(() => {
+    setKeywordByDate(splitKeywordByDate(data.items));
+  }, [data]);
+
   return (
     <Box>
-      {data.items.map((keyword, idx) => (
-        <Accordion 
-          expanded={expanded === idx}
-          onChange={handleChange(idx, keyword.title)}
+      {Object.keys(keywordByDate).map((date, idx) => (
+        <Box 
           key={idx}
+          sx={{
+            mb: 2
+          }}
         >
-          {(expanded !== idx) && 
-          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography sx={{ width: '33%', flexShrink: 0 }}>{keyword.title}</Typography>
-            <Typography sx={{ flexShrink: 2, color: 'text.secondary' }}>{keyword.traffic}</Typography>
-            <Typography sx={{ flexShrink: 2, color: 'text.secondary' }}>{fromNow(keyword.isoDate)}</Typography>
-          </AccordionSummary>}
-          {(expanded === idx) && 
-          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography sx={{ width: '80%', flexShrink: 2 }}>{keyword.title}</Typography>
-            {/* <Typography sx={{ color: 'text.secondary' }}>{fromNow(keyword.pubDate)}</Typography> */}
-          </AccordionSummary>}
-          <AccordionDetails>
-            <Box>
-              {keyword.description.split(",").map((word, idx) => (
-                <Button key={idx}>{word}</Button>
-                ))
+          <Typography variant="h5">{moment(date).format('YYYY년 MM월 DD일')}</Typography>
+          {keywordByDate[date].map((keyword, idx) => (
+            <Accordion 
+              expanded={expanded === keyword.title}
+              onChange={handleChange(keyword.title)}
+              key={idx}
+            >
+              {(expanded !== keyword.title) && 
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                  <Typography variant="h6" sx={{ width: '50%', flexShrink: 0 }}>{keyword.title}</Typography>
+                  <Typography sx={{ display:'flex', color: 'text.secondary' }}>{keyword.traffic}</Typography>
+                  <Typography sx={{ display:'flex', color: 'text.secondary' }}>{fromNow(keyword.isoDate)}</Typography>
+                </AccordionSummary>
               }
-              <img alt="thumbnail" src={keyword.picture} />
-            </Box>
-            {/* <Box>
-              {data}
-            </Box> */}
-          </AccordionDetails>
-        </Accordion>
+              {(expanded === keyword.title) && 
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                  <Typography sx={{ width: '80%', flexShrink: 2 }}>{keyword.title}</Typography>
+                </AccordionSummary>
+              }
+              <AccordionDetails>
+                <Box>
+                  {keyword.description.split(",").map((word, idx) => (
+                    <Button key={idx}>{word}</Button>
+                    ))
+                  }
+                  <img alt="thumbnail" src={keyword.picture} />
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+          ))}
+        </Box>
       ))}
     </Box>
   )
--- client/src/Routes/Trends.js
+++ client/src/Routes/Trends.js
@@ -1,13 +1,12 @@
 import React, { Suspense } from 'react'
 import KeywordsList from '../Components/KeywordList';
-import { Container, Typography } from '@mui/material';
+import { Container } from '@mui/material';
 import { ErrorBoundary } from 'react-error-boundary';
 import KeywordListLoading from '../Components/KeywordListLoading';
 
 const Trends = () => {
   return (
     <Container>
-      <Typography variant="h4" sx={{ mb: 2 }}>인기 검색어</Typography>
       <Suspense fallback={<KeywordListLoading/>}>
         <ErrorBoundary FallbackComponent={<div>Error!</div>}>
           <KeywordsList />
