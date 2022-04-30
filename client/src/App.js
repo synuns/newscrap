876feb97a10a555396f144f0cb97cf71943ec70f
Jdentity <identityjang@gmail.com>
2022-04-30 17:11:41 +0900

    ✅ Add scroll to top in the Routes
    Add scroll to top component Wrapper

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useLayoutEffect } from 'react';
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 import NavBar from "./Components/NavBar";
 import News from "./Routes/News";
@@ -6,17 +7,27 @@ import Search from "./Routes/Search";
 import Scrap from "./Routes/Scrap";
 import Trends from "./Routes/Trends";
 
+const Wrapper = ({children}) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children
+} 
+
 function App() {
   return (
     <BrowserRouter>
-      <CssBaseline />
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<News />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/trends" element={<Trends />} />
-        <Route path="/scrap" element={<Scrap />} />
-      </Routes>
+      <Wrapper>
+        <CssBaseline />
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<News />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/trends" element={<Trends />} />
+          <Route path="/scrap" element={<Scrap />} />
+        </Routes>
+      </Wrapper>
     </BrowserRouter>
   );
 }
