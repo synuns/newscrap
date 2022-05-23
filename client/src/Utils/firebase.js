39323e8f1543b7789643ab2039b470da90e29910
Jdentity <identityjang@gmail.com>
2022-05-24 00:04:26 +0900

    ⚗️ Experiment firebase

---
+++ client/src/Utils/firebase.js
@@ -0,0 +1,20 @@
+// import * as firebase from "firebase";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FB_DATABASE_URL,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_API_ID,
+  measurementId: process.env.REACT_APP_FB_MEASUERMENT_ID
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
+export {firebaseApp, db};
