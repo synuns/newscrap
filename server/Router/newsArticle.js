b73ba0906df8770b88dc8e8a8a1f38c8ac76fede
Jdentity <identityjang@gmail.com>
2022-05-23 15:31:10 +0900

    ♻️ Refactor newsParser

--- server/Router/newsArticle.js
+++ server/Router/newsArticle.js
@@ -4,16 +4,44 @@ const router = express.Router();
 const charset = require('charset');
 const HTMLParser = require('node-html-parser');
 const articleParser = require('article-parser');
-const https = require('https');
 const axios = require('axios');
 
 axios.default.withCredentials = true;
 
-const msnNewsParser = (headers, body) => {
-  let enc = charset(headers, body);
+const encodeHTML = (headers, body) => {
+  const enc = charset(headers, body);
+  const encodedHTML = body.toString(enc);
+  return encodedHTML;
+}
 
-  const i_result = body.toString(enc);
-  const root = HTMLParser.parse(i_result);
+const getHTML = async(url) => {
+  const config = {
+    method: 'get',
+    url: url,
+    encoding : 'utf-8',
+    headers : { 
+      'content-type': 'application/x-www-form-urlencoded',
+    },
+  };
+
+  const result = await axios(config)
+    .then((response) => {
+      const html = encodeHTML(response.headers, response.data);
+      return html;
+    })
+    .catch((error) => {
+      return error;
+    });
+  
+  return result;
+};
+
+const cutQueryFromUrl = (url) => {
+  return url.replace(/\?.*/g,'');
+}
+
+const getMsnArticle = (html) => {
+  const root = HTMLParser.parse(html);
 
   const articleContent = root.querySelector('.articlecontent');
 
@@ -23,8 +51,7 @@ const msnNewsParser = (headers, body) => {
   const providerLogo = partner.querySelector('img').getAttribute('src');
   const published = articleContent.querySelector('.time').innerText;
   const author = articleContent.querySelector('.truncate').innerHTML;
-  // const image = root.querySelector('span.image').querySelector('img').getAttribute('src');
-  // const imageCaption = root.querySelector('caption').innerText;
+  const image = root.querySelector('span.image').querySelector('img').getAttribute('src');
   const content = root.querySelectorAll('div.richtext');
 
   const article = {
@@ -33,8 +60,7 @@ const msnNewsParser = (headers, body) => {
     providerLogo,
     published,
     author,
-    // image,
-    // imageCaption,
+    image,
     content,
   };
 
@@ -42,56 +68,41 @@ const msnNewsParser = (headers, body) => {
     article[key] = article[key].toString();
   });
 
+  // 이미지 src 쿼리값 제거
+  article.providerLogo = cutQueryFromUrl(article.providerLogo);
+  article.image = cutQueryFromUrl(article.image);
+  
   return article;
 }
 
-const getMsnArticle = async (url) => {
-  const httpsAgent = new https.Agent({ keepAlive: true });
-  const config = {
-    httpsAgent,
-    method: 'get',
-    url: url,
-    encoding : null,
-    headers : { 
-      'content-type': 'application/x-www-form-urlencoded',
-    },
-  };
-
-  const result = await axios(config)
-    .then((response) => {
-      const article = msnNewsParser(response.headers, response.data);
-      // console.log(response.data);
-      // console.log(article);
-      // return response.data;
-      return article;
+const getArticle = (html) => {
+  const article = articleParser.extract(html)
+    .then((data) => {
+      return data;
     })
     .catch((error) => {
       return error;
-    });
-  
-  return result;
-}
-
-const getArticle = async (url) => {
-  try {
-    const article = await articleParser.extract(url)
-    return article;
-  } catch (error) {
-    console.trace(err)
-    return error;
-  }
+    })
+  return article; 
 }
 
 router.post("/", (req, res) => {
   const url = req.body.link;
-  const result = url.includes('www.msn.com') ? getMsnArticle(url) : getArticle(url);
-  result
+
+  getHTML(url)
     .then((data) => {
-      res.send(data);
+      if(url.includes('www.msn.com')){
+        res.send(getMsnArticle(data));
+      } else{
+        return getArticle(data);
+      }
+    })
+    .then((article) => {
+      res.send(article);
     })
     .catch((error) => {
       res.send(error);
-    });
+    })
 });
 
 module.exports = router;
