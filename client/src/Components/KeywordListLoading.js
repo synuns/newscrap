f537a07eb7609673d60f8cac2757f72d3919a056
Jdentity <identityjang@gmail.com>
2022-05-28 18:34:00 +0900

    🩹 fix loading component

--- client/src/Components/KeywordListLoading.js
+++ client/src/Components/KeywordListLoading.js
@@ -1,25 +1,11 @@
-import { Accordion, Box, Skeleton } from '@mui/material';
+import { Box } from '@mui/material';
 import React from 'react';
 
 const KeywordListLoading = () => {
-  let skeletons = [];
-
-  const skeleton = (key) => (
-    <Accordion 
-      key={key} 
-      sx={{ height: '40px'}}
-    >
-      <Skeleton sx={{ mx: 2, height: '100%' }} />
-    </Accordion>
-  );
-
-  for(let i=0; i<20; i++){
-    skeletons.push(skeleton(i));
-  };
 
   return (
     <Box>
-      {skeletons}
+      loading...
     </Box>
   );
 }
